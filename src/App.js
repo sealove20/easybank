50279babf0ce33f6dffe30f516a683fce78f4c09
Matheus Henrique <matheus.181137@gmail.com>
2020-11-12 22:13:28 -0300

    Created component for about_card section, WIP about_card prop types, tittle position

--- src/App.js
+++ src/App.js
@@ -12,20 +12,6 @@ function App() {
       <Main />
       <About />
   
-      Simple Budgeting
-      See exactly where your money goes each month. Receive notifications when you’re 
-      close to hitting your limits.
-  
-      Fast Onboarding
-      We don’t do branches. Open your account in minutes online and start taking control 
-      of your finances right away.
-  
-      Open API
-      Manage your savings, investments, pension, and much more from one account. Tracking 
-      your money has never been easier.
-  
-    
-  
       Latest Articles
   
       By Claire Robinson
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import AboutCard from './components/AboutCard';
 
-import onlines from '../../assets/images/icon-online.svg'
+import onlines from '../../assets/images/icon-online.svg';
+import budgeting from '../../assets/images/icon-budgeting.svg';
+import onboarding from '../../assets/images/icon-onboarding.svg';
+import api from '../../assets/images/icon-api.svg';
 
 import './styles.scss';
 
@@ -13,15 +17,31 @@ export default function About() {
           We leverage Open Banking to turn your bank account into your financial hub. Control 
           your finances like never before.
         </p>
-        <section className="about__card">
-          <img src={onlines} alt="icon with a hand holding plastics" />
-          <h3 className="about__card__tittle">
-            Online Banking
-          </h3>
-          <p className="about__card__text">
-            Our modern web and mobile applications allow you to keep track of your finances 
-            wherever you are in the world.
-          </p>
+        <section className="about__card__container">
+          <AboutCard 
+            cardImage={onlines}
+            cardTittle="Online Banking" 
+            cardText="Our modern web and mobile applications allow you to keep track of your finances 
+            wherever you are in the world." 
+          />
+          <AboutCard 
+            cardImage={budgeting} 
+            cardTittle="Simple Budgeting" 
+            cardText="See exactly where your money goes each month. Receive notifications when you’re 
+            close to hitting your limits." 
+          />
+          <AboutCard 
+            cardImage={onboarding} 
+            cardTittle="Fast Onboarding" 
+            cardText="We don’t do branches. Open your account in minutes online and start taking control 
+            of your finances right away." 
+          />
+          <AboutCard 
+            cardImage={api} 
+            cardTittle="Open API" 
+            cardText="Manage your savings, investments, pension, and much more from one account. Tracking 
+            your money has never been easier." 
+          />
         </section>
       </section>
     </article>
